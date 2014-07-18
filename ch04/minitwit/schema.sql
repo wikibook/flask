ea28118fd0eaa4b6971bfd57a5420c1a5ba49844
keaton <keatonh@gmail.com>
2014-07-18 19:43:27 +0900

    - to initialize user/follower/message tables automatically, add init_db() before app.run()
    - update sql not to initialize the tables when running minitwit again.

--- ch04/minitwit/schema.sql
+++ ch04/minitwit/schema.sql
@@ -1,19 +1,18 @@
-drop table if exists user;
-create table user (
+create table if not exists user (
   user_id integer primary key autoincrement,
   username string not null,
   email string not null,
   pw_hash string not null
 );
 
-drop table if exists follower;
-create table follower (
+
+create table if not exists follower (
   who_id integer,
   whom_id integer
 );
 
-drop table if exists message;
-create table message (
+
+create table if not exists message (
   message_id integer primary key autoincrement,
   author_id integer not null,
   text string not null,
